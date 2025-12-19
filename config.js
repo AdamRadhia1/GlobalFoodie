3e7d476ee46bbb0a30b716d5a0b23dff78587f4b
Radhia Adam <radhia.adam@gwu.edu>
2025-12-19 18:48:55 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Mapbox access token for local development. Do not commit a production token in a public repo.
+window.MAPBOX_TOKEN = 'pk.eyJ1IjoiYWRhbXJhZGhpYTEiLCJhIjoiY21pcm9wb250MDJvajNjbjUzZzlxenp3bCJ9.0g6OE5Sig5wxzpyICUBfqw';
+const FOURSQUARE_API_KEY = "3HBVRU2AECI2Q5USK1TIA2B3VORABWXDSDP4T0XUG5PTNPBX";
